3ca609d70de6270e27a80362aa17b50be51ad9dc
Ibrahim elabdellaoui <92434976+Elabdellaou@users.noreply.github.com>
2022-10-09 11:54:13 +0100

    Update mail.php

--- mail.php
+++ mail.php
@@ -14,10 +14,10 @@ $mail = new PHPMailer(true);
 //Server settings
 //$mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
 $mail->isSMTP();                                            //Send using SMTP
-$mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+$mail->Host       = '';                     //Set the SMTP server to send through
 $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-$mail->Username   = 'ibraelabde@gmail.com';                     //SMTP username
-$mail->Password   = 'ibraikram';                               //SMTP password
+$mail->Username   = '';                     //SMTP username
+$mail->Password   = '';                               //SMTP password
 $mail->SMTPSecure = 'ssl';            //Enable implicit TLS encryption
 $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
